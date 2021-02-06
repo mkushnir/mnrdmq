e5a8c03564b650d96974d7bc021de028bc6a220f
Markiyan Kushnir <kushnir@samawatt.com>
2021-02-06 19:16:55 +0200

    devt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='mnrdmq',
     python_requires='>=3.7.0',
-    version='0.1.0',
+    version='0.1.1',
     license='BSD',
     packages=find_packages(),
     #include_package_data=True,
