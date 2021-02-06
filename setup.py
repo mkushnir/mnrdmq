ed4a0c021292cbc8878e41e21a5634bad73cac0c
Markiyan Kushnir <kushnir@samawatt.com>
2021-02-06 19:05:41 +0200

    devt

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mnrdmq',
+    python_requires='>=3.7.0',
+    version='0.1.0',
+    license='BSD',
+    packages=find_packages(),
+    #include_package_data=True,
+    long_description="Simple controller-agent framework",
+    install_requires=open("requirements.txt").read()
+)
