01efec5d2a07110fb7159e4a2cfc2c2359ccf9d8
Markiyan Kushnir [at home] <mkushnir@gmail.com>
2022-01-15 16:16:36 +0200

    0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='mnrdmq',
     python_requires='>=3.7.0',
-    version='0.1.3',
+    version='0.1.5',
     license='BSD',
     packages=find_packages(),
     #include_package_data=True,
